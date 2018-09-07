eb219681a8a4f9202abdbf5796edcdb68a4195b4
Nimrod Abrigo <abrigonimrod@gmail.com>
2018-09-07 18:06:42 +0700

    add libraries
    - angular material
    - simple grid CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
