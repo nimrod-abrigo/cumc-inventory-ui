c15b835bb30146e6072a3b12a1eda26d6ffb91ca
Nimrod O. Abrigo <abrigonimrod@gmail.com>
2018-09-17 09:56:34 +0800

    event-item template
    initial design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatNativeDateModule, MatDatepickerModule, MatIconModule, MatTooltipModule, MatListModule, MatDialogModule, MatInputModule, MatSnackBarModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatNativeDateModule, MatDatepickerModule, MatIconModule, MatTooltipModule, MatListModule, MatDialogModule, MatInputModule, MatSnackBarModule, MatExpansionModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -23,6 +23,7 @@ import { AppRoutingModule, routingComponents } from './/app-routing.module';
     BrowserAnimationsModule,
     MatButtonModule,
     MatNativeDateModule,
+    MatExpansionModule,
     MatCheckboxModule,
     MatToolbarModule,
     MatInputModule,
