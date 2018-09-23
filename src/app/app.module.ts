a5609385f27c44271ea224323087961664c790b5
Nimrod Abrigo <abrigonimrod@gmail.com>
2018-09-23 22:53:40 +0700

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule, MatNativeDateModule, MatDatepickerModule, MatIconModule, MatTooltipModule, MatListModule, MatDialogModule, MatInputModule, MatSnackBarModule, MatExpansionModule, MatTableModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatOptionModule, MatCardModule, MatNativeDateModule, MatDatepickerModule, MatIconModule, MatTooltipModule, MatListModule, MatDialogModule, MatInputModule, MatSnackBarModule, MatExpansionModule, MatTableModule, MatOption, MatSelectModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -25,6 +25,8 @@ import { AppRoutingModule, routingComponents } from './/app-routing.module';
     MatNativeDateModule,
     MatExpansionModule,
     MatCheckboxModule,
+    MatOptionModule,
+    MatSelectModule,
     MatTableModule,
     MatToolbarModule,
     MatInputModule,
